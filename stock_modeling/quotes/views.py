ba4563e8043b738b4069482b86811a1c82be435f
dorrero <saivamshi.samala@gmail.com>
2020-10-03 12:59:08 +0530

    First commit

---
+++ stock_modeling/quotes/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+	import requests
+	import json
+
+	# NJFJY7DW1WXTJ4U4 -- alphavantage
+	if request.method == 'POST':
+		ticker = request.POST['ticker']
+		api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_164c554030a54634b6851c5dec4dbe97")
+		#api_request = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GOOGL&apikey=NJFJY7DW1WXTJ4U4")
+
+		try:
+			api = json.loads(api_request.content)
+		except Exception as e:
+			api = "Error..."
+		return render(request, 'home.html', {'api': api})
+		
+	else:
+		return render(request, 'home.html', {'ticker': "Enter a ticker symbol above."})
+
+def about(request):
+	return render(request, 'about.html', {})
