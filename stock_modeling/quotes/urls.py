d4b82a418e0b13dc98b5faeadd339b19e6cdbc84
PreethamBojja <bojjasaipreetham@gmail.com>
2020-10-07 12:11:55 +0530

    Form with ticker and period added

--- stock_modeling/quotes/urls.py
+++ stock_modeling/quotes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	path('', views.home, name="home"),
 	path('about.html', views.about, name="about"),
+	path('form.html', views.form, name="form"),
 ]
--- stock_modeling/quotes/views.py
+++ stock_modeling/quotes/views.py
@@ -22,3 +22,6 @@ def home(request):
 
 def about(request):
 	return render(request, 'about.html', {})
+
+def form(request):
+	return render(request, 'form.html', {})
